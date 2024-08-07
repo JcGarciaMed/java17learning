5703b9bf30ea50a2cf762965753dc1f6cacc9c05
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2024-08-07 17:05:56 -0500

    Supplier Interface

---
+++ src/functionalintefaces/SupplierClass.java
@@ -0,0 +1,27 @@
+package functionalintefaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierClass {
+    public static void main(String[] args) {
+        List<Alumno> alumnos = Arrays.asList(
+                new Alumno(20, "Juan"),
+                new Alumno(18, "Carlos"),
+                new Alumno(10, "Pedro"));
+
+        Supplier<Alumno> s2 = () -> new Alumno(15, "Jorge");
+        System.out.println(getAlumno(s2));
+
+        Supplier<Alumno> s3 = () -> {
+            alumnos.get(0).setNota(12);
+            return alumnos.get(0);
+        };
+        System.out.println(s3.get());
+    }
+
+    public static Alumno getAlumno(Supplier<Alumno> s){
+        return s.get();
+    }
+}
